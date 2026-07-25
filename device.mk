adc642e7f5921123607bf17f0f837e02e0fba305
RodrigoAyuso <rodrigo.mazzonetti@gmail.com>
2026-07-25 20:53:41 -0300

    Hawaiip: Minor adjustments.

--- device.mk
+++ device.mk
@@ -7,6 +7,12 @@
 
 LOCAL_PATH := device/motorola/hawaiip
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
